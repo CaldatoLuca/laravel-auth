d856b43bb11f5e2e379e9defaba7cdfa3e841e5d
CaldatoLuca <lucacaldato15@gmail.com>
2024-02-10 13:38:18 +0100

    feat: commenti

--- resources/js/functionality/image_preview.js
+++ resources/js/functionality/image_preview.js
@@ -1,21 +1,29 @@
+//prendo gli elementi e do un html di default
 let imageElement = document.querySelector("#upload-image");
 let imagePreview = document.querySelector("#image-preview");
 let hasImage = document.querySelector(".has-image");
 hasImage.innerHTML = "No Image Selected";
 imagePreview.innerHTML = '<i class="fa-solid fa-x"></i>';
 
+//funzione al change del valore del type input
 imageElement.addEventListener("change", function () {
-    const file = this.files[0]; // Ottieni il primo file selezionato
+    const file = this.files[0]; // seleziono il file caricato
+    //se il file esiste
     if (file) {
         const reader = new FileReader(); // Crea un oggetto FileReader
 
+        // evento onload chiamato quando il file viene letto
         reader.onload = function (event) {
-            // L'evento onload viene chiamato quando il file è stato letto
-            const imageUrl = event.target.result; // Ottieni l'URL dell'immagine
-            imagePreview.innerHTML = `<img class="image rounded-2" src="${imageUrl}" alt="Anteprima dell'immagine">`; // Mostra l'anteprima dell'immagine
+            // trasformo la path in un url
+            const imageUrl = event.target.result;
+            //mostro immagine
+            imagePreview.innerHTML = `<img class="image rounded-2" src="${imageUrl}" alt="Anteprima dell'immagine">`;
         };
 
-        reader.readAsDataURL(file); // Leggi il file come URL di dati
+        // Legge il file come URL di dati
+        reader.readAsDataURL(file);
+
+        //cambio html e style in modo da vedere la preview e non il placeholder
         hasImage.innerHTML = "Image Preview";
         imagePreview.classList.remove(
             "image-placeholder",
@@ -25,7 +33,7 @@ imageElement.addEventListener("change", function () {
         );
         imagePreview.classList.add("overflow-hidden", "image-show");
     } else {
-        imagePreview.innerHTML = ""; // Pulisci l'anteprima se nessun file è selezionato
+        //cambio html e style in modo da vedere il placeholder e non la preview
         hasImage.innerHTML = "No Image Selected";
         imagePreview.innerHTML = '<i class="fa-solid fa-x"></i>';
         imagePreview.classList.remove("overflow-hidden", "image-show");
@@ -36,9 +44,3 @@ imageElement.addEventListener("change", function () {
         );
     }
 });
-
-//immagine
-//rounded-2 overflow-hidden image-show
-
-//placeholder
-//image-placeholder d-flex justify-content-center align-items-center rounded-2 bg-danger
